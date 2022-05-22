e161dd08907f3b97f829a32f3374ea90e594db8e
Ben Hunt <bnjmnhnt.git@gmail.com>
2022-05-21 20:07:39 -0600

    initial commit

---
+++ scripts/list-sessions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+list() {
+	choice=$(tmux list-sessions |
+		sed -E 's/:.*$//' |
+		grep -v \"^$(tmux display-message -p '#S')\$\" |
+		fzf --reverse --prompt="session:")
+
+	# exit gracefully with no tmux complaint
+	[ -z $choice ] && exit 0
+
+	tmux switch-client -t $choice
+}
+
+list
