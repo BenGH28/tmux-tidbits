864d5018631779416b54ab209b02cd9e19cc51b9
Ben Hunt <bnjmnhnt.git@gmail.com>
2023-12-16 23:56:08 -0700

    chore: formatting

--- scripts/list-sessions.sh
+++ scripts/list-sessions.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 list() {
-	choice=$(tmux list-sessions |
-		sed -E 's/:.*$//' |
-		grep -v \"^$(tmux display-message -p '#S')\$\" |
-		fzf --reverse --prompt="session:")
+    choice=$(tmux list-sessions \
+        | sed -E 's/:.*$//' \
+        | grep -v \"^$(tmux display-message -p '#S')\$\" \
+        | fzf --reverse --prompt="session:")
 
-	# exit gracefully with no tmux complaint
-	[ -z $choice ] && exit 0
+    # exit gracefully with no tmux complaint
+    [ -z $choice ] && exit 0
 
-	tmux switch-client -t $choice
+    tmux switch-client -t $choice
 }
 
 list
--- scripts/new-project.sh
+++ scripts/new-project.sh
@@ -7,33 +7,33 @@ project_name=""
 repo_path=""
 
 get_project_name() {
-	read -p "Enter new project name: " project_name
-	# sanitize it for unwanted characters
-	project_name=${project_name//+([^A-Za-z0-9])/-}
+    read -p "Enter new project name: " project_name
+    # sanitize it for unwanted characters
+    project_name=${project_name//+([^A-Za-z0-9])/-}
 }
 
 make_project_repo() {
-	repo_path="$1/$project_name"
-	mkdir -p $repo_path
-	curr_dir=$(pwd)
-	cd $repo_path
-	git init
-	cd $curr_dir
+    repo_path="$1/$project_name"
+    mkdir -p $repo_path
+    curr_dir=$(pwd)
+    cd $repo_path
+    git init
+    cd $curr_dir
 }
 
 create_new_session() {
-	tmux new-session -d -c $repo_path -s $project_name
+    tmux new-session -d -c $repo_path -s $project_name
 }
 
 switch_to_session() {
-	tmux switch-client -t $project_name
+    tmux switch-client -t $project_name
 }
 
 new() {
-	get_project_name
-	make_project_repo $1
-	create_new_session
-	switch_to_session
+    get_project_name
+    make_project_repo $1
+    create_new_session
+    switch_to_session
 }
 
 new $1
